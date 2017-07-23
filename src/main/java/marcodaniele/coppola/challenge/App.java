3efeb22fded0ca37fe400db23e186f5e23cb681c
Marco Daniele Coppola <marcodaniele.coppola@techedgegroup.com>
2017-07-23 09:48:11 +0000

    first commit

---
+++ src/main/java/marcodaniele/coppola/challenge/App.java
@@ -0,0 +1,13 @@
+package marcodaniele.coppola.challenge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
